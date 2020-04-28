571cd217303763f64f6cb8d2641c5853d0f7043f
Marcelo <marcelo@marcelos-pro.home>
2020-04-28 15:51:32 -0300

    SearchBar added and Posts updated in forum.dart

--- lib/pages/forum.dart
+++ lib/pages/forum.dart
@@ -16,7 +16,7 @@ final Forum = Scaffold(
                     Align(
                       alignment: Alignment.centerLeft,
                       child: Text(
-                        'Notícias Flow',
+                        'Forum',
                         style: TextStyle(
                           color: Colors.black,
                           fontWeight: FontWeight.w600,
@@ -24,15 +24,12 @@ final Forum = Scaffold(
                         ),
                       ),
                     ),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Acompanhe o Twitter do Flowpodcast',
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 14,
-                        ),
+                    SizedBox(height: 5),
+                    Container(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Pesquise no forum'),
                       ),
                     ),
                   ],
@@ -45,7 +42,7 @@ final Forum = Scaffold(
                     Align(
                       alignment: Alignment.centerLeft,
                       child: Text(
-                        'Twitter Oficial',
+                        'Trending hoje',
                         style: TextStyle(
                           color: Colors.black,
                           fontWeight: FontWeight.w600,
@@ -101,6 +98,36 @@ final Forum = Scaffold(
                               color: Colors.lightBlue[50],
                               borderRadius: BorderRadius.circular(12),
                             ),
+                            child: Column(
+                              children: <Widget>[
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      'Flow Podcast',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    Text(
+                                      '13 de jan',
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 5),
+                                Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    'não é uma entrevista, é uma conversa',
+                                    style: TextStyle(
+                                        //fontWeight: FontWeight.w600,
+                                        ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
@@ -109,7 +136,7 @@ final Forum = Scaffold(
                     Align(
                       alignment: Alignment.centerLeft,
                       child: Text(
-                        'Comunidade Youtube',
+                        'Popular posts',
                         style: TextStyle(
                           color: Colors.black,
                           fontWeight: FontWeight.w600,
@@ -162,6 +189,100 @@ final Forum = Scaffold(
                         ],
                       ),
                     ), //
+                    SizedBox(height: 25),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Recentes',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 20),
+                          Container(
+                            padding: const EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              color: Colors.lightBlue[50],
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      'Flow Podcast',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    Text(
+                                      '13 de jan',
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 5),
+                                Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    'não é uma entrevista, é uma conversa',
+                                    style: TextStyle(
+                                        //fontWeight: FontWeight.w600,
+                                        ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Container(
+                            padding: const EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              color: Colors.lightBlue[50],
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      'Flow Podcast',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    Text(
+                                      '13 de jan',
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 5),
+                                Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    'não é uma entrevista, é uma conversa',
+                                    style: TextStyle(
+                                        //fontWeight: FontWeight.w600,
+                                        ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
